9c5d7b3451e9cc319a9b257d063d3610549b985a
Cyrus Wise <147113250+cyrwise@users.noreply.github.com>
2024-07-01 22:29:02 -0700

    Create main.cpp

---
+++ Homework/#01 (C++) - Sales Price Calculator/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+float amogus, bruh, cartoose, dogecoin, eucalyptus;
+
+cout << "Sale Price Calculator" << endl;
+cout << "" << endl;
+cout << "What is the regular price? ";
+cin >> amogus;
+cout << "What is the percentage off? ";
+cin >> bruh;
+
+cartoose = bruh / 100;
+dogecoin = amogus * cartoose;
+eucalyptus = amogus - dogecoin;
+
+cout << "" << endl;
+cout << "The sale price is: " << eucalyptus;
+}
